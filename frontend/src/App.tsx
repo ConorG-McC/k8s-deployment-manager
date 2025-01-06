040b444d7bc62ad5e705a77269608d7b9af1e533
Conor Giffen-McCloskey <conorgiffenmccloskey@gmail.com>
2025-01-06 04:57:01 +0000

    Implement routing for deployment and progress pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import DeploymentForm from './components/DeploymentForm';
+import DeploymentForm from './components/deploymentForm/DeploymentForm';
 import './styles/common.css';
 import './App.css';
-import DeploymentProgress from './components/DeploymentProgress';
+import DeploymentProgress from './components/deploymentProgressPage/DeploymentProgress';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/homePage/HomePage';
 
 function App(): React.ReactElement {
   const [deploymentId, setDeploymentId] = useState<string | null>(null);
@@ -18,11 +20,26 @@ function App(): React.ReactElement {
       <div className='build-info-watermark'>
         <span>[version number]</span>
       </div>
-      {!deploymentId ? (
-        <DeploymentForm onSubmit={setDeploymentId} />
-      ) : (
-        <DeploymentProgress deploymentId={deploymentId} />
-      )}
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route
+          path='/deploy'
+          element={<DeploymentForm onSubmit={setDeploymentId} />}
+        />
+        <Route
+          path='/progress/:deploymentId'
+          element={
+            deploymentId ? (
+              <DeploymentProgress deploymentId={deploymentId} />
+            ) : (
+              // <div>Loading...</div>
+              <DeploymentProgress deploymentId='1' />
+            )
+          }
+        />
+        {/* Add a fallback route for undefined paths */}
+        <Route path='*' element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
